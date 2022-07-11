1dde102987af027d93c34e2df3b942ddae237ab3
Anthony <anthonynwobodo7@gmail.com>
2022-07-11 21:27:09 +0100

    :bug: (env fixes)

--- server.ts
+++ server.ts
@@ -1,8 +1,20 @@
 import express from 'express';
+import 'dotenv/config';
 import { postgraphile } from 'postgraphile';
 
 const app = express();
 
-let server = app.listen(process.env.PORT || 3000 , () => {
+app.use(
+    postgraphile(
+        process.env.DATABASE_URL, 
+        process.env.DATABASE_SCHEMA, 
+        {
+            watchPg: true,
+            graphiql: true,
+            enhanceGraphiql: true,
+        })
+)
+
+app.listen(process.env.PORT || 3000 , () => {
   console.log('Server is running on port 3000');
 });
